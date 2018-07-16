a574248625da7c05c4308d632ee20d570541654a
Carlos Castillo (ChaTo) <chato@chato.cl>
2018-07-16 16:47:35 +0200

    New simulation params

--- src/post_processing_methods/fair_ranker/simulation_yang_stoyanovich.py
+++ src/post_processing_methods/fair_ranker/simulation_yang_stoyanovich.py
@@ -3,6 +3,7 @@
 import random
 from scipy.stats import binom
 
+
 def create_ranking_yang_stoyanovich(p, k):
     ranking = []
     for i in range(k):
@@ -24,21 +25,12 @@ def calculate_protected_needed_at_each_position(p, k, alpha):
     return result
 
 
-
-# In[83]:
-
-
 def count_protected_in_prefix(k, ranking):
     count = 0
     for i in range(k):
         if ranking[i] == 1:
             count += 1
     return count
-
-print(count_protected_in_prefix(2, [0, 1, 1, 1]))
-
-
-# In[84]:
 
 
 def ranking_satisfies_table_at_every_position(ranking, table):
@@ -65,11 +57,13 @@ def perform_simulation_yang_stoyanovich(N, p, k, alpha):
 
 print("Example ranking")
 print(create_ranking_yang_stoyanovich(0.2, 10))
+print("Example count protected on script")
+print(count_protected_in_prefix(2, [0, 1, 1, 1]))
 print("Example mtable")
 print(calculate_protected_needed_at_each_position(0.6, 12, 0.1))
 print("Example ranking satisfies mtable")
 print(ranking_satisfies_table_at_every_position([0, 1, 1, 0, 1], [0, 1, 2, 2, 3]))
 
 print("** Starting simulations **")
-perform_simulation_yang_stoyanovich(N=10000, p=0.8, k=1000, alpha=0.01)
-perform_simulation_yang_stoyanovich(N=10000, p=0.8, k=1500, alpha=0.05)
+perform_simulation_yang_stoyanovich(N=10000, p=0.5, k=1000, alpha=0.01)
+#perform_simulation_yang_stoyanovich(N=10000, p=0.5, k=1500, alpha=0.05)
