82a2528d5105d066476ad1502571a348ef065751
Carlos Castillo (ChaTo) <chato@chato.cl>
2018-08-14 19:05:20 +0200

    Unit tests and methods to run the simulation

---
+++ src-python/cc_run_simulation_yang_stoyanovich.py
@@ -0,0 +1,15 @@
+
+from simulation_yang_stoyanovich import perform_simulation_yang_stoyanovich
+
+import numpy as np
+
+N = 10000
+print("N=%d (number of trials)" % N)
+print()
+
+for (k, alpha) in (1000, 0.01), (1500, 0.05):
+    print(" k=%d, alpha=%.2f" % (k, alpha))
+    for p in np.arange(0.1, 0.9, 0.1):
+        failure_rate = perform_simulation_yang_stoyanovich(N=N, p=p, k=k, alpha=alpha, quiet=True)
+        print("   p=%.2f failure_rate=%.6f" % (p, failure_rate))
+    print()
--- src-python/simulation_yang_stoyanovich.py
+++ src-python/simulation_yang_stoyanovich.py
@@ -2,7 +2,6 @@
 
 import random
 from scipy.stats import binom
-import csv
 
 
 def create_ranking_yang_stoyanovich(p, k):
@@ -17,18 +16,6 @@ def create_ranking_yang_stoyanovich(p, k):
         else:
             ranking.append(0)
     return ranking
-
-
-def calculate_protected_needed_at_each_position(p, k, alpha):
-    ''' Calculate an mtable for parameters p, k, alpha
-    '''
-    result = []
-
-    for n in range(1, k + 1):
-        numProtCandidates = binom.ppf(alpha, n, p)
-        result.append(int(numProtCandidates))
-
-    return result
 
 
 def count_protected_in_prefix(k, ranking):
@@ -53,70 +40,45 @@ def ranking_satisfies_table_at_every_position(ranking, table):
     return (True)
 
 
-def perform_simulation_yang_stoyanovich(N, p, k, alpha):
+def calculate_protected_needed_at_each_position(p, k, alpha):
+    ''' Calculate an mtable for parameters p, k, alpha
+    '''
+    result = []
+
+    for n in range(1, k + 1):
+        numProtCandidates = binom.ppf(alpha, n, p)
+        result.append(int(numProtCandidates))
+
+    return result
+
+
+def proportion_false(results):
+    ''' Determine the fraction of results that were false
+    '''
+
+    failures = 0
+    trials = len(results)
+    for result in results:
+        if not result:
+            failures += 1
+
+    return float(failures)/float(trials)
+
+
+def perform_simulation_yang_stoyanovich(N, p, k, alpha, quiet=False):
     ''' Perform a simulation by generating N rankings using Yang-Stoyanovich
         method, and then compare them with an mtable.
     '''
     mtable = calculate_protected_needed_at_each_position(p, k, alpha)
 
-    success = 0
+    results = []
     for i in range(N):
         ranking = create_ranking_yang_stoyanovich(p, k)
-        if ranking_satisfies_table_at_every_position(ranking, mtable):
-            success += 1
-        if (i + 1) % 100 == 0:
-            print("trial %d/%d: p=%.2f, k=%d, alpha=%.2f; success=%d, failure_prob=%.4f" %
-                  (i, N, p, k, alpha, success, (i - success) / success))
+        result = ranking_satisfies_table_at_every_position(ranking, mtable)
+        results.append(result)
+        if not quiet:
+            if (i + 1) % 100 == 0:
+                print("trial %d/%d: p=%.2f, k=%d, alpha=%.2f; failure_prob=%.4f" %
+                      (i, N, p, k, alpha, proportion_false(results)))
 
-
-def writeMTableToCSV(k, p, alpha):
-    mtable = calculate_protected_needed_at_each_position(p, k, alpha)
-    filename = "mtable_" + repr(k) + "_" + repr(p) + "_" + repr(alpha) + ".csv"
-    with open("C:\\Users\\TSuehr\\Documents\\synthetic_fair_test\\csvFiles\\csv-python\\" + filename,
-              "w") as csv_file:
-        writer = csv.writer(csv_file, delimiter=';')
-        writer.writerow(mtable)
-
-
-def writeRankingToCSV(k, p, alpha):
-    ranking = create_ranking_yang_stoyanovich(p, k)
-    filename = "ranking_" + repr(k) + "_" + repr(p) + "_" + repr(alpha) + ".csv"
-    with open("C:\\Users\\TSuehr\\Documents\\synthetic_fair_test\\csvFiles\\csv-python\\"
-              + filename,
-              "w") as csv_file:
-        writer = csv.writer(csv_file, delimiter=';')
-        writer.writerow(ranking)
-
-
-def getProportionFromCSVFile(k, p, alpha):
-    filename = "ranking_" + repr(k) + "_" + repr(p) + "_" + repr(alpha) + ".csv"
-    with open("C:\\Users\\TSuehr\\Documents\\synthetic_fair_test\\csvFiles\\csv-python\\"
-              + filename,
-              "r") as csv_file:
-        reader = csv.reader(csv_file, delimiter=';')
-        counter = 0
-        for row in reader:
-            for i in row:
-                counter += int(i)
-    return counter/k
-
-k = 1500
-alpha = 0.05
-for p in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-    #writeMTableToCSV(k, p, alpha)
-    #writeRankingToCSV(k, p, alpha)
-    print(getProportionFromCSVFile(k, p, alpha))
-# print("Example ranking")
-# print(create_ranking_yang_stoyanovich(0.2, 10))
-# print("Example count protected on script")
-# print(count_protected_in_prefix(2, [0, 1, 1, 1]))
-# print("Example mtable")
-# print(calculate_protected_needed_at_each_position(0.6, 12, 0.1))
-# print("Example ranking satisfies mtable")
-# print(ranking_satisfies_table_at_every_position([0, 1, 1, 0, 1], [0, 1, 2, 2, 3]))
-
-# print("** Starting simulations **")
-# Blue curve (top) in Figure 2 of FA*IR
-# perform_simulation_yang_stoyanovich(N=10000, p=0.5, k=1500, alpha=0.05)
-# Red curve (bottom) in Figure 2 of FA*IR
-# perform_simulation_yang_stoyanovich(N=10000, p=0.5, k=1000, alpha=0.01)
+    return proportion_false(results)
