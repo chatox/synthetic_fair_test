626528c8bf4b024dd93859f170e6fd08a53cdf23
Tom Sühr <tom.suehr@googlemail.com>
2018-09-05 14:51:00 +0200

    added the length of the last block into the auxMTable calculation

--- src-java/analyticalVSexperimental/Main.java
+++ src-java/analyticalVSexperimental/Main.java
@@ -4,12 +4,18 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException {
-        int k = 100;
+        int k = 500;
         //double[] pValues = {0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9};
-        double p = 0.5;
-        double alpha = 0.05;
+        double p = 0.9;
+        double alpha = 0.1;
+        Simulator sim = new Simulator(10000, k,p,alpha);
+        System.out.println(sim.run());
+        MTableMultiTester tester = new MTableMultiTester(k,p,alpha);
+        MTableGenerator generator = new MTableGenerator(k,p,alpha);
+        int[] mtable = generator.getMTable();
+        System.out.println(tester.computeFailureProbability(mtable));
 
-        MTableMultiTester analyticalProcedure = new MTableMultiTester(k, p, alpha);
+        /*MTableMultiTester analyticalProcedure = new MTableMultiTester(k, p, alpha);
 
         ArrayList<MTableFailProbPair> analytical = analyticalProcedure.createDescendingMTables();
         ArrayList<Double> experimental = new ArrayList<>();
@@ -17,10 +23,10 @@ public class Main {
         for(MTableFailProbPair pair : analytical){
             Simulator sim = new Simulator(5000, k,p,pair.getAlpha());
             experimental.add(sim.run());
-        }
+        }*/
 
-        CSVWriter writer = new CSVWriter();
-        writer.writeMTableFailProbPairAndSimulationFailProbPairToCSV(analytical, experimental);
+        //CSVWriter writer = new CSVWriter();
+        //writer.writeMTableFailProbPairAndSimulationFailProbPairToCSV(analytical, experimental);
 
     }
 }
