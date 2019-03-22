9b5c203ad677230285f29e978e884c86b3f17a21
Tom Sühr <t.suehr@campus.tu-berlin.de>
2019-03-22 10:04:10 +0100

    started analytical failprobability calculation

--- src-java/multinomial/MultinomialSimulator.java
+++ src-java/multinomial/MultinomialSimulator.java
@@ -25,7 +25,7 @@ public class MultinomialSimulator {
     private double[] p;
     private double alpha;
     private TreeNode<int[]> multinomialMtable;
-    HashMap<int[], TreeNode<int[]>> obtainedPossibilities = new HashMap<>();
+    private MCDFCache mcdfCache;
 
 
     public MultinomialSimulator(int runs, int k, double[] p, double alpha) {
@@ -33,8 +33,7 @@ public class MultinomialSimulator {
         this.k = k;
         this.p = p;
         this.alpha = alpha;
-//        this.multinomialMtable = computeMultinomialMtables();
-//        completeMtable();
+        this.mcdfCache = new MCDFCache(k,p,alpha);
     }
 
     private ArrayList<Integer> createRanking(int k) {
@@ -65,7 +64,7 @@ public class MultinomialSimulator {
                 seenSoFar[ranking.get(i)]++;
             }
             seenSoFar[0] = i + 1;
-            if (MultinomialDist.cdf(i + 1, p, seenSoFar) <= alpha) {
+            if (mcdfCache.mcdf(seenSoFar) <= alpha) {
 //                System.out.println("["+seenSoFar[0]+","+seenSoFar[1]+","+seenSoFar[2]+"]");
                 return false;
             }
@@ -138,39 +137,6 @@ public class MultinomialSimulator {
         return true;
     }
 
-//    private void completeMtable() {
-//        ArrayList<TreeNode<int[]>> nextLevel = new ArrayList<>();
-//        nextLevel.add(this.multinomialMtable);
-//        while (true) {
-//            ArrayList<TreeNode<int[]>> children = new ArrayList<>();
-//            for (TreeNode<int[]> t : nextLevel) {
-//                children.addAll(t.children);
-//            }
-//            //make additional edges
-//            for (TreeNode<int[]> c : children) {
-//                for (TreeNode<int[]> t : children) {
-//                    if (c.id != t.id && FairnessTestGraph.isSameSignature(c.data, t.data)) {
-//                        if (c.children.size() == 0) {
-//                            for (TreeNode<int[]> p : c.parent) {
-//                                p.addChild(t);
-//                            }
-//                        } else {
-//                            for (TreeNode<int[]> p : t.parent) {
-//                                p.addChild(c);
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//            ///////////////
-//
-//            nextLevel = children;
-//            if (nextLevel.size() == 0) {
-//                return;
-//            }
-//        }
-//    }
-
     public double run(int k) {
         int successes = 0;
         for (int i = 0; i < runs; i++) {
@@ -186,13 +152,11 @@ public class MultinomialSimulator {
             }
         }
 
-//        System.out.println(1 - (double) successes / runs);
-
         return 1 - (double) successes / runs;
     }
 
-    private boolean containsSignature(ArrayList<int[]> list, int[] arr) {
-        for (int[] sig : list) {
+    private boolean containsSignature(HashMap<int[],TreeNode<int[]>> list, int[] arr) {
+        for (int[] sig : list.keySet()) {
             int counter = 0;
             for (int i = 0; i < sig.length; i++) {
                 if (sig[i] == arr[i]) {
@@ -210,28 +174,31 @@ public class MultinomialSimulator {
         int[] currentProtectedCount = new int[this.p.length];
         TreeNode<int[]> root = new TreeNode<>(currentProtectedCount);
         ArrayList<TreeNode<int[]>> openPossibilities = new ArrayList<>();
-        double currentTime = System.nanoTime();
         openPossibilities.add(root);
+        double currentTime = System.nanoTime();
+        int currentLevel = 1;
         while (openPossibilities.size() > 0) {
             ArrayList<TreeNode<int[]>> intermediatePossibilities = new ArrayList<>();
             ArrayList<TreeNode> validPossibilities = new ArrayList<>();
-            ArrayList<int[]> seenSoFar = new ArrayList<>();
+            HashMap<int[],TreeNode<int[]>> seenSoFar = new HashMap<>();
             for (TreeNode<int[]> t : openPossibilities) {
-                if (!containsSignature(seenSoFar, t.data)) {
-                    seenSoFar.add(t.data);
+                if (seenSoFar.get(t.data)!=null) {
+                    seenSoFar.put(t.data,t);
                     validPossibilities.add(t);
+                }else{
+                    TreeNode<int[]> node = seenSoFar.get(t.data);
+                    node.weight += t.weight;
                 }
+
             }
-//            this.obtainedPossibilities = new HashMap<>();
-//            System.gc();
             for (TreeNode<int[]> t : validPossibilities) {
                 inverseMultinomialCDF(t);
                 intermediatePossibilities.addAll(t.children);
             }
 
-
-            System.out.println((System.nanoTime() - currentTime) / (1000000000) + ";" + openPossibilities.get(0).getLevel() + ";" + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (double) (1024 * 1024));
             openPossibilities = intermediatePossibilities;
+            System.out.println((System.nanoTime() - currentTime) / (1000000000) + ";" + currentLevel + ";" + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (double) (1024 * 1024));
+            currentLevel ++;
             currentTime = System.nanoTime();
             System.gc();
         }
@@ -247,26 +214,21 @@ public class MultinomialSimulator {
         int[] minProportions = new int[currentNode.data.length];
         System.arraycopy(currentNode.data, 0, minProportions, 0, currentNode.data.length);
         minProportions[0]++;
-        double mcdf = MultinomialDist.cdf(trials, this.p, minProportions);
+        double mcdf = mcdfCache.mcdf(minProportions);
         if (mcdf > alpha) {
             TreeNode<int[]> child = currentNode.addChild(minProportions);
             child.cdf = mcdf;
+            child.weight = currentNode.weight;
         } else {
             for (int i = 1; i < this.p.length; i++) {
                 int[] temp = new int[minProportions.length];
                 System.arraycopy(minProportions, 0, temp, 0, minProportions.length);
                 temp[i]++;
-                double mcdfTemp = MultinomialDist.cdf(trials, this.p, temp);
+                double mcdfTemp = mcdfCache.mcdf(temp);
                 if (mcdfTemp > alpha) {
-//                    if(obtainedPossibilities.containsKey(temp)){
-//                        TreeNode<int[]> child = obtainedPossibilities.get(temp);
-//                        currentNode.addChild(child);
-//                    }else{
                     TreeNode<int[]> child = currentNode.addChild(temp);
-//                        obtainedPossibilities.put(temp,child);
                     child.cdf = mcdfTemp;
-//                    }
-
+                    child.weight = currentNode.weight;
                 }
             }
         }
@@ -277,12 +239,12 @@ public class MultinomialSimulator {
         String filePrefix = "node_";
         int numberOfNodes = directory.list().length;
 
-        HashMap<Integer,NodeWriteObject> nodes = new HashMap<>();
+        HashMap<Integer, NodeWriteObject> nodes = new HashMap<>();
         for (int i = 0; i < numberOfNodes; i++) {
-            FileInputStream fi = new FileInputStream(directory+"\\"+filePrefix+i);
+            FileInputStream fi = new FileInputStream(directory + "\\" + filePrefix + i);
             ObjectInputStream oi = new ObjectInputStream(fi);
             NodeWriteObject node = (NodeWriteObject) oi.readObject();
-            nodes.put(node.id,node);
+            nodes.put(node.id, node);
             oi.close();
             fi.close();
         }
@@ -317,14 +279,178 @@ public class MultinomialSimulator {
     public static void main(String[] args) throws Exception {
         double[] p = {1.0 / 3.0, 1.0 / 3.0, 1.0 / 3.0};
         int k = 500;
-        MultinomialSimulator simulator = new MultinomialSimulator(10000, 500, p, 0.1);
-        simulator.readNodes("C:\\Users\\Tom\\Desktop\\CIT\\mtable500\\");
+        MultinomialSimulator simulator = new MultinomialSimulator(10000,250,p,0.1);
+        simulator.computeMultinomialMtables();
+        ////////////////////////////////////////////////////////////////////////////////////
+        //level 1, 2 failprob = 0;
+
+        double failprobOnLevel2 = 0;
+        double succProbOnLevel2 = 1-failprobOnLevel2;
+
+        //NodesOnLevel 3
+
+        int[] n1 = {3,1,0};
+        int[] n2 = {3,0,1};
+        //Parent from level 2
+        int[] parent = {2,0,0};
+        //wird zu
+        int[] n1Converted = {3,0,0};
+        int[] n2Converted = {3,0,0};
+        // 1 gleiches Paar = 1/1
+        double failprobOnLevel3 = 1*( 0.5*succProbOnLevel2* MultinomialDist.cdf(3,p,n1Converted) + 0.5*succProbOnLevel2* MultinomialDist.cdf(3,p,n2Converted));
+        double succProbOnLevel3 = 1-failprobOnLevel3;
+        System.out.println("Level 3 " + failprobOnLevel3);
+        //NodesOnLevel 4
+        int[] n3 = {4,2,0};
+        int[] n4 = {4,1,1};
+        int[] n5 = {4,1,1};
+        int[] n6 = {4,0,2};
+        //Parents from level 3
+        int[] p1 = {3,1,0};
+        int[] p2 = {3,0,1};
+        //wird zu
+        int[] n3Converted = {4,1,0};
+        int[] n4Converted = {4,1,0};
+        int[] n5Converted = {4,0,1};
+        int[] n6Converted = {4,0,1};
+        //2 gleiche Paare = 1/2
+        double failprobOnLevel4 = 0.5*succProbOnLevel3*MultinomialDist.cdf(4,p,n3Converted)
+                + 0.5*succProbOnLevel3 * MultinomialDist.cdf(4,p,n4Converted)
+                + 0.5*succProbOnLevel3 * MultinomialDist.cdf(4,p,n5Converted)
+                + 0.5*succProbOnLevel3 * MultinomialDist.cdf(4,p,n6Converted);
+        double succProbOnLevel4 = 1-failprobOnLevel4;
+        System.out.println("Level 4 " + failprobOnLevel4);
+        //NodesOnLevel 5
+        int[] n7 = {5,3,0};
+        int[] n8 = {5,2,1};
+        int[] n9 = {5,1,1};
+        int[] n10 = {5,1,1};
+        int[] n11 = {5,1,2};
+        int[] n12 = {5,0,3};
+        //Parents from level 4
+        int[] p3 = {4,2,0};
+        int[] p4 = {4,1,1};
+        int[] p5 = {4,1,1};
+        int[] p6 = {4,0,2};
+        //wird zu
+        int[] n7Converted = {5,2,0};
+        int[] n8Converted = {5,2,0};
+        int[] n9Converted = {5,1,1};
+        int[] n10Converted = {5,1,1};
+        int[] n11Converted = {5,0,2};
+        int[] n12Converted = {5,0,2};
+        //Ein gleiches Parent Paar / drei gleiche Paare Converted = 1/3
+        double failProbOnLevel5 = (1.0/3.0)*(succProbOnLevel4*MultinomialDist.cdf(5,p,n7Converted)
+                + succProbOnLevel4*MultinomialDist.cdf(5,p,n8Converted)
+                + succProbOnLevel4*MultinomialDist.cdf(5,p,n9Converted)
+                + succProbOnLevel4*MultinomialDist.cdf(5,p,n10Converted)
+                + succProbOnLevel4*MultinomialDist.cdf(5,p,n11Converted)
+                + succProbOnLevel4*MultinomialDist.cdf(5,p,n12Converted));
+        double succProbOnLevel5 = 1-failProbOnLevel5;
+        System.out.println("Level 5 " + failProbOnLevel5);
+        //NodesOnLevel 6
+        int[] n13 = {6,3,1};
+        int[] n14 = {6,2,1};
+        int[] n15 = {6,2,1};
+        int[] n16 = {6,1,2};
+        int[] n17 = {6,2,1};
+        int[] n18 = {6,1,2};
+        int[] n19 = {6,1,2};
+        int[] n20 = {6,1,3};
+        //Parents from level 5
+        int[] p7 = {5,3,0};
+        int[] p8 = {5,2,1};
+        int[] p9 = {5,1,1};
+        int[] p10 = {5,1,1};
+        int[] p11 = {5,1,2};
+        int[] p12 = {5,0,3};
+        //wird zu
+        int[] n13Converted = {6,3,0};
+        int[] n14Converted = {6,2,1};
+        int[] n15Converted = {6,1,1};
+        int[] n16Converted = {6,1,1};
+        int[] n17Converted = {6,1,1};
+        int[] n18Converted = {6,1,1};
+        int[] n19Converted = {6,1,2};
+        int[] n20Converted = {6,0,3};
+        //Ein gleiches Parent Paar /zwei gleiche Paare in Converted = 1/2
+        double failProbOnLevel6 =0.5*(succProbOnLevel5*MultinomialDist.cdf(6,p,n13Converted)
+                + succProbOnLevel5*MultinomialDist.cdf(6,p,n14Converted)
+                + succProbOnLevel5*MultinomialDist.cdf(6,p,n15Converted)
+                + succProbOnLevel5*MultinomialDist.cdf(6,p,n16Converted)
+                + succProbOnLevel5*MultinomialDist.cdf(6,p,n17Converted)
+                + succProbOnLevel5*MultinomialDist.cdf(6,p,n18Converted)
+                + succProbOnLevel5*MultinomialDist.cdf(6,p,n19Converted)
+                + succProbOnLevel5*MultinomialDist.cdf(6,p,n20Converted));
+        double succProbOnLevel6 = 1-failProbOnLevel6;
+        System.out.println("Level 6 " + failProbOnLevel6);
+        //NodesOnLevel 7
+        int[] n21 = {7,3,1};
+        int[] n22 = {7,3,1};
+        int[] n23 = {7,2,2};
+        int[] n24 = {7,3,1};
+        int[] n25 = {7,2,2};
+        int[] n26 = {7,2,2};
+        int[] n27 = {7,1,3};
+        int[] n28 = {7,3,1};
+        int[] n29 = {7,2,2};
+        int[] n30 = {7,2,2};
+        int[] n31 = {7,1,3};
+        int[] n32 = {7,2,2};
+        int[] n33 = {7,1,3};
+        int[] n34 = {7,1,3};
+        //Parents from level 6
+        int[] p13 = {6,3,1};
+        int[] p14 = {6,2,1};
+        int[] p15 = {6,2,1};
+        int[] p16 = {6,1,2};
+        int[] p17 = {6,2,1};
+        int[] p18 = {6,1,2};
+        int[] p19 = {6,1,2};
+        int[] p20 = {6,1,3};
+        //wird zu
+        int[] n21Converted = {7,3,1};
+
+        int[] n22Converted = {7,2,1};
+        int[] n23Converted = {7,2,1};
+
+        int[] n24Converted = {7,2,1};
+        int[] n25Converted = {7,2,1};
+
+        int[] n26Converted = {7,1,2};
+        int[] n27Converted = {7,1,2};
+
+        int[] n28Converted = {7,2,1};
+        int[] n29Converted = {7,2,1};
+
+        int[] n30Converted = {7,1,2};
+        int[] n31Converted = {7,1,2};
+
+        int[] n32Converted = {7,1,2};
+        int[] n33Converted = {7,1,2};
+
+        int[] n34Converted = {7,1,3};
+
+        //Zwei gleiche Parent Paar /6 gleiche Paare Converted = 2/6
+        double failProbOnLevel7 = (2.0/6.0) * (succProbOnLevel6*MultinomialDist.cdf(7,p,n21Converted) + succProbOnLevel6*MultinomialDist.cdf(7,p,n22Converted)
+                + succProbOnLevel6*MultinomialDist.cdf(7,p,n23Converted)
+                + succProbOnLevel6*MultinomialDist.cdf(7,p,n24Converted)+ succProbOnLevel6*MultinomialDist.cdf(7,p,n25Converted)+ succProbOnLevel6*MultinomialDist.cdf(7,p,n26Converted)
+                + succProbOnLevel6*MultinomialDist.cdf(7,p,n27Converted)+ succProbOnLevel6*MultinomialDist.cdf(7,p,n28Converted)+ succProbOnLevel6*MultinomialDist.cdf(7,p,n29Converted)
+                + succProbOnLevel6*MultinomialDist.cdf(7,p,n30Converted)+ succProbOnLevel6*MultinomialDist.cdf(7,p,n31Converted)+ succProbOnLevel6*MultinomialDist.cdf(7,p,n32Converted)
+                + succProbOnLevel6*MultinomialDist.cdf(7,p,n33Converted)+ succProbOnLevel6*MultinomialDist.cdf(7,p,n34Converted));
+        System.out.println("Level 7 "+failProbOnLevel7);
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+        //save and mark nodes which are counted x times like weights
+
+//        simulator.readNodes("C:\\Users\\Tom\\Desktop\\CIT\\mtable500\\");
 //        simulator.computeMultinomialMtables();
 //        System.out.println(simulator.run());
 //        simulator.writeNodesToFile("C:\\Users\\Tom\\Desktop\\CIT\\mtable500\\");
-        for(int i=1; i<=k; i++){
-            System.out.println(i+";"+simulator.run(i));
-        }
+//        for(int i=1; i<=k; i++){
+//            System.out.println(i+";"+simulator.run(i));
+//        }
 //            String treeName = "test";
 //            MultinomialSimulator simulator = new MultinomialSimulator(10000, 10, p, 0.1);
 //            simulator.run();
--- src-java/multinomial/util/TreeNode.java
+++ src-java/multinomial/util/TreeNode.java
@@ -5,124 +5,129 @@ import java.util.*;
 
 public class TreeNode<T> implements Iterable<TreeNode<T>>, Serializable {
 
-	public T data;
-	public List<TreeNode<T>> parent;
-	public List<TreeNode<T>> children;
-	public double cdf;
-	public int id;
-	public static int ID = 0;
+    public T data;
+    public List<TreeNode<T>> parent;
+    public List<TreeNode<T>> children;
+    public double cdf;
+    public int id;
+    public static int ID = 0;
+    public int weight = 1;
 
-	public static TreeNode<int[]> recreateTreeNode(HashMap<Integer, NodeWriteObject> nodes) {
-		TreeNode<int[]> root = null;
-		ArrayList<NodeWriteObject> nextLevelChildren = new ArrayList<>();
-		ArrayList<TreeNode<int[]>> lastLevelNodes = new ArrayList<>();
-		for(NodeWriteObject n : nodes.values()){
-			if(n.level == 0){
-				root = new TreeNode<>(n.data);
-				root.cdf = n.cdf;
-				root.id = n.id;
-				lastLevelNodes.add(root);
-				for(int id : n.children){
-					nextLevelChildren.add(nodes.get(id));
-				}
-				break;
-			}
-		}
-		while(nextLevelChildren.size()>0){
-			ArrayList<NodeWriteObject> nextLevel = new ArrayList<>();
-			ArrayList<TreeNode<int[]>> lastLevel = new ArrayList<>();
-			for(NodeWriteObject n : nextLevelChildren){
-				TreeNode<int[]> parent;
-				for(TreeNode<int[]> p : lastLevelNodes){
-					if(n.parents.contains(p.id)){
-						parent =p;
-						TreeNode<int[]> child = parent.addChild(n.data);
-						child.cdf = n.cdf;
-						child.id = n.id;
-						lastLevel.add(child);
-						for(int id : n.children){
-							nextLevel.add(nodes.get(id));
-						}
-						break;
-					}
-				}
-			}
-			nextLevelChildren = nextLevel;
-			lastLevelNodes = lastLevel;
-		}
+    public static TreeNode<int[]> recreateTreeNode(HashMap<Integer, NodeWriteObject> nodes) {
+        TreeNode<int[]> root = null;
+        ArrayList<NodeWriteObject> nextLevelChildren = new ArrayList<>();
+        ArrayList<TreeNode<int[]>> lastLevelNodes = new ArrayList<>();
+        for (NodeWriteObject n : nodes.values()) {
+            if (n.level == 0) {
+                root = new TreeNode<>(n.data);
+                root.cdf = n.cdf;
+                root.id = n.id;
+                lastLevelNodes.add(root);
+                for (int id : n.children) {
+                    nextLevelChildren.add(nodes.get(id));
+                }
+                break;
+            }
+        }
+        while (nextLevelChildren.size() > 0) {
+            ArrayList<NodeWriteObject> nextLevel = new ArrayList<>();
+            ArrayList<TreeNode<int[]>> lastLevel = new ArrayList<>();
+            for (NodeWriteObject n : nextLevelChildren) {
+                TreeNode<int[]> parent;
+                for (TreeNode<int[]> p : lastLevelNodes) {
+                    if (n.parents.contains(p.id)) {
+                        parent = p;
+                        TreeNode<int[]> child = parent.addChild(n.data);
+                        child.cdf = n.cdf;
+                        child.id = n.id;
+                        lastLevel.add(child);
+                        for (int id : n.children) {
+                            nextLevel.add(nodes.get(id));
+                        }
+                        break;
+                    }
+                }
+            }
+            nextLevelChildren = nextLevel;
+            lastLevelNodes = lastLevel;
+        }
 
-		return root;
-	}
+        return root;
+    }
 
-	public boolean isRoot() {
-		return parent.size() == 0;
-	}
+    public boolean isSplit() {
+        return children.size() > 1;
+    }
 
-	public boolean isLeaf() {
-		return children.size() == 0;
-	}
+    public boolean isRoot() {
+        return parent.size() == 0;
+    }
 
-	private List<TreeNode<T>> elementsIndex;
+    public boolean isLeaf() {
+        return children.size() == 0;
+    }
 
-	public TreeNode(T data) {
-		this.id = ID;
-		ID++;
-		this.data = data;
-		this.parent = new LinkedList<TreeNode<T>>();
-		this.children = new LinkedList<TreeNode<T>>();
-		this.elementsIndex = new LinkedList<TreeNode<T>>();
-		this.elementsIndex.add(this);
-	}
+    private List<TreeNode<T>> elementsIndex;
 
-	public TreeNode<T> addChild(T child) {
-		TreeNode<T> childNode = new TreeNode<T>(child);
-		childNode.parent.add(this);
-		this.children.add(childNode);
-		this.registerChildForSearch(childNode);
-		return childNode;
-	}
+    public TreeNode(T data) {
+        this.id = ID;
+        ID++;
+        this.data = data;
+        this.parent = new LinkedList<TreeNode<T>>();
+        this.children = new LinkedList<TreeNode<T>>();
+        this.elementsIndex = new LinkedList<TreeNode<T>>();
+        this.elementsIndex.add(this);
+    }
 
-	public TreeNode<T> addChild(TreeNode<T> child){
-		child.parent.add(this);
-		this.children.add(child);
-		return child;
-	}
+    public TreeNode<T> addChild(T child) {
+        TreeNode<T> childNode = new TreeNode<T>(child);
+        childNode.parent.add(this);
+        this.children.add(childNode);
+        this.registerChildForSearch(childNode);
+        return childNode;
+    }
 
-	public int getLevel() {
-		if (this.isRoot())
-			return 0;
-		else
-			return parent.get(0).getLevel() + 1;
-	}
+    public TreeNode<T> addChild(TreeNode<T> child) {
+        child.parent.add(this);
+        this.children.add(child);
+        return child;
+    }
 
-	private void registerChildForSearch(TreeNode<T> node) {
-		elementsIndex.add(node);
-	}
+    public int getLevel() {
+        if (this.isRoot())
+            return 0;
+        else
+            return parent.get(0).getLevel() + 1;
+    }
 
-	public TreeNode<T> findTreeNode(Comparable<T> cmp) {
-		for (TreeNode<T> element : this.elementsIndex) {
-			T elData = element.data;
-			if (cmp.compareTo(elData) == 0)
-				return element;
-		}
+    private void registerChildForSearch(TreeNode<T> node) {
+        elementsIndex.add(node);
+    }
 
-		return null;
-	}
+    public TreeNode<T> findTreeNode(Comparable<T> cmp) {
+        for (TreeNode<T> element : this.elementsIndex) {
+            T elData = element.data;
+            if (cmp.compareTo(elData) == 0)
+                return element;
+        }
 
-	@Override
-	public String toString() {
-		return data != null ? data.toString() : "[data null]";
-	}
+        return null;
+    }
 
-	@Override
-	public Iterator<TreeNode<T>> iterator() {
-		TreeNodeIter<T> iter = new TreeNodeIter<T>(this);
-		return iter;
-	}
+    @Override
+    public String toString() {
+        return data != null ? data.toString() : "[data null]";
+    }
 
-	public void disconnect() {
-		for(TreeNode<T> parent : this.parent){
-			parent.children.remove(this);
-		}
-	}
+    @Override
+    public Iterator<TreeNode<T>> iterator() {
+        TreeNodeIter<T> iter = new TreeNodeIter<T>(this);
+        return iter;
+    }
+
+    public void disconnect() {
+        for (TreeNode<T> parent : this.parent) {
+            parent.children.remove(this);
+        }
+    }
 }
