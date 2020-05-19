b3f678d687fc733a7cad29c53f4e672bcc7f4dde
Meike Zehlike <meike.zehlike@tu-berlin.de>
2020-05-19 16:22:54 +0200

    done clean compas data

--- src-python/multinomial/cleanCompasData.py
+++ src-python/multinomial/cleanCompasData.py
@@ -4,11 +4,12 @@ Created on May 19, 2020
 @author: meike
 '''
 import pandas as pd
+import numpy as np
 import os
+import scipy.stats as stats
 
 
 def main():
-    print(os.getcwd())
     data = pd.read_csv("../../src-java/multinomial/data/COMPAS/compas-scores-two-years.csv", header=0)
     # we do the same data cleaning as is done by ProPublica. See link for details
     # https://github.com/propublica/compas-analysis/blob/master/Compas%20Analysis.ipynb
@@ -19,7 +20,7 @@ def main():
     data = data[data["score_text"] != "N/A"]
 
     # drop irrelevant columns
-    keep_cols = ["sex", "age_cat", "race", "decile_score", "v_decile_score"]
+    keep_cols = ["sex", "age_cat", "race", "decile_score", "v_decile_score", "priors_count"]
     data = data[keep_cols]
     data["sex"] = data["sex"].replace({"Male":0,
                                        "Female":1})
@@ -32,6 +33,25 @@ def main():
                                          "Asian":3,
                                          "Native American":4,
                                          "Other":5})
+    # calculate score based on recidivism score, violent recidivism score and number of prior arrests
+    # violent recidivism weighs highest.
+    data["score"] = np.zeros(data.shape[0])
+    for idx, row in data.iterrows():
+        recidivism = row.loc["decile_score"]
+        violentRecidivism = row.loc["v_decile_score"]
+        priorArrests = row.loc["priors_count"]
+
+        score = 0.25 * recidivism + 0.5 * violentRecidivism + 0.25 * priorArrests
+        data.loc[idx, "score"] = score
+
+    data["score"] = data["score"].max() - data["score"]
+    print(data["score"])
+    # drop these columns
+    data = data.drop(columns=['decile_score', 'v_decile_score', 'priors_count'])
+
+    # zscore score and normalize scores
+    data["score"] = stats.zscore(data["score"])
+
     data.to_csv("../../src-java/multinomial/data/COMPAS/compas_sexAgeRace.csv", header=True, index=False)
 
 
